83c7e6ceae0f4a8c4ec5ec60feb51f4d17dd2607
raks030517 <raks030517@gmail.com>
2026-05-22 14:18:02 +0900

    멀티페이지 섹션 나누기

--- app(ex).py
+++ app(ex).py
@@ -12,7 +12,7 @@
 
 import streamlit as st
 overview = st.Page("session1.py", title="요약", icon="📊")
-detail   = st.Page("session2.py", title="상세 분석", icon="🔍")
+detail = st.Page("session2.py", title="상세 분석", icon="🔍")
 setting = st.Page("session3.py", title="설정")
 
 pg = st.navigation({"분석": [overview, detail], "설정": [setting]})
